989beda195ef705987dc34e144859f41a1712c61
Daksh Dixit <dakshdixit183@gmail.com>
2024-12-29 23:22:24 +0530

    misc

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,8 @@
-import { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
