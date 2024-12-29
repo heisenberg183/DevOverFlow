79559dc134fde3db9afe6b39f86c98fccc1f13ad
Daksh Dixit <dakshdixit183@gmail.com>
2024-12-29 23:20:08 +0530

    ESLint and Prettier Config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -10,7 +11,49 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: [
+      "next/core-web-vitals",
+      "next/typescript",
+      "plugin:unicorn/recommended",
+      "plugin:import/recommended",
+      "plugin:playwright/recommended",
+      "plugin:prettier/recommended",
+      "prettier",
+    ],
+    plugins: ["simple-import-sort"],
+    rules: {
+      camelcase: [0, { properties: "never" }],
+      "no-undef": "off",
+      "simple-import-sort/exports": "error",
+      "simple-import-sort/imports": "error",
+      "unicorn/no-array-callback-reference": "off",
+      "unicorn/no-array-for-each": "off",
+      "unicorn/no-array-reduce": "off",
+      "unicorn/prevent-abbreviations": [
+        "error",
+        {
+          allowList: {
+            e2e: true,
+          },
+          replacements: {
+            props: false,
+            ref: false,
+            params: false,
+          },
+        },
+      ],
+    },
+    ignorePatterns: ["components/ui/**"],
+    overrides: [
+      {
+        files: ["*.ts", "*.tsx"],
+        rules: {
+          "no-undef": "off",
+        },
+      },
+    ],
+  }),
 ];
 
 export default eslintConfig;
